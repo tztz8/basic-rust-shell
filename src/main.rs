3da37550f8ea3aa86327bc7ea70a73365939637c
Timbre Freeman <timbre@tftinker.tech>
2024-05-24 15:20:02 -0700

    missing command

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,31 @@ enum ShellCommandType {
 }
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
 
-    let input = input.trim();
-    let end_command_index = input.find(' ').unwrap_or(input.len());
-    let (command_part, _args_part) = input.split_at(end_command_index);
+        let input = input.trim();
+        let end_command_index = input.find(' ').unwrap_or(input.len());
+        let (command_part, _args_part) = input.split_at(end_command_index);
 
-    let input_command_type = ShellCommandType::Unknow;
+        let input_command_type = ShellCommandType::Unknow;
 
-    match input_command_type {
-        ShellCommandType::Shell => {
-            todo!();
-        }
-        ShellCommandType::Unknow => {
-            println!("{}: command not found", command_part);
-        }
-        ShellCommandType::Program => {
-            todo!();
+        match input_command_type {
+            ShellCommandType::Shell => {
+                todo!();
+            }
+            ShellCommandType::Unknow => {
+                println!("{}: command not found", command_part);
+            }
+            ShellCommandType::Program => {
+                todo!();
+            }
         }
     }
 }
