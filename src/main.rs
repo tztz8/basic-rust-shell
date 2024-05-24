cf56d4e5e3f7f1d2b1c636027c40e1a06f40c25c
Timbre Freeman <timbre@tftinker.tech>
2024-05-24 15:16:57 -0700

    missing command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
+
+#[allow(dead_code)]
+enum ShellCommandType {
+    Shell,
+    Program,
+    Unknow,
+}
 
 fn main() {
     print!("$ ");
@@ -9,4 +15,22 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    let input = input.trim();
+    let end_command_index = input.find(' ').unwrap_or(input.len());
+    let (command_part, _args_part) = input.split_at(end_command_index);
+
+    let input_command_type = ShellCommandType::Unknow;
+
+    match input_command_type {
+        ShellCommandType::Shell => {
+            todo!();
+        }
+        ShellCommandType::Unknow => {
+            println!("{}: command not found", command_part);
+        }
+        ShellCommandType::Program => {
+            todo!();
+        }
+    }
 }
