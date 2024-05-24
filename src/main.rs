355ca8a67817922ec122f7fd8ecad84e4177afe8
Timbre Freeman <timbre@tftinker.tech>
2024-05-24 15:48:31 -0700

    exit command

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,26 @@ use std::io::{self, Write};
 
 #[allow(dead_code)]
 enum ShellCommandType {
-    Shell,
+    Shell(ShellCommands),
     Program,
     Unknow,
+}
+
+#[derive(Debug)]
+enum ShellCommands {
+    Exit,
+}
+
+impl ShellCommands {
+    const VALUES: [Self; 1] = [Self::Exit];
+}
+
+impl std::fmt::Display for ShellCommands {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ShellCommands::Exit => write!(f, "exit"),
+        }
+    }
 }
 
 fn main() {
@@ -19,14 +36,29 @@ fn main() {
 
         let input = input.trim();
         let end_command_index = input.find(' ').unwrap_or(input.len());
-        let (command_part, _args_part) = input.split_at(end_command_index);
+        let (command_part, args_part) = input.split_at(end_command_index);
+        let args_part = args_part.trim();
 
-        let input_command_type = ShellCommandType::Unknow;
+        let mut input_command_type = ShellCommandType::Unknow;
+
+        for shell_command in ShellCommands::VALUES {
+            if shell_command.to_string().eq(command_part) {
+                input_command_type = ShellCommandType::Shell(shell_command);
+                break;
+            }
+        }
 
         match input_command_type {
-            ShellCommandType::Shell => {
-                todo!();
-            }
+            ShellCommandType::Shell(shell_command) => match shell_command {
+                ShellCommands::Exit => {
+                    let is_args_emp = args_part.is_empty();
+                    let mut exit_code: i32 = args_part.parse().unwrap_or(-1);
+                    if is_args_emp {
+                        exit_code = 0;
+                    }
+                    std::process::exit(exit_code);
+                }
+            },
             ShellCommandType::Unknow => {
                 println!("{}: command not found", command_part);
             }
