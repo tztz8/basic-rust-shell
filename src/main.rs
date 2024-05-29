6c0d3031a810e5bb5b97ca6b76985c6b70fc0647
Timbre Freeman <timbre@tftinker.tech>
2024-05-29 15:21:16 -0700

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -129,89 +129,96 @@ fn main() {
         // run command
         match pase_command_type(&paths, command_part) {
             ShellCommandType::Shell(shell_command) => match shell_command {
-                ShellCommands::Exit => {
-                    let is_args_emp = args_part.is_empty();
-                    let mut exit_code: i32 = args_part.parse().unwrap_or(-1);
-                    if is_args_emp {
-                        exit_code = 0;
-                    }
-                    std::process::exit(exit_code);
-                }
-                ShellCommands::Echo => {
-                    println!("{}", args_part);
-                }
-                ShellCommands::Type => {
-                    let arg_command_type = pase_command_type(&paths, args_part);
-                    match arg_command_type {
-                        ShellCommandType::Unknow => {
-                            println!("{}: not found", args_part);
-                        }
-                        ShellCommandType::Program(path) => {
-                            let path = absolute_path(&(std::path::Path::new(&path))).unwrap();
-                            println!("{} is {}", args_part, path.to_str().unwrap());
-                        }
-                        ShellCommandType::Shell(_) => {
-                            println!("{} is a shell builtin", args_part);
-                        }
-                    }
-                }
-                ShellCommands::PrintWorkingDirectory => {
-                    let current_dir = std::env::current_dir();
-                    if current_dir.is_err() {
-                        eprintln!("Can't get current working directory: {:?}", current_dir.unwrap_err());
-                    } else {
-                        let current_dir = current_dir.unwrap();
-                        println!("{}", current_dir.to_str().unwrap_or("UNKNOWN"));
-                    }
-                }
-                ShellCommands::ChangeDirectory => {
-                    let mut path = std::path::PathBuf::new();
-                    path.push(args_part);
-                    // may need to be joined to working dir
-                    if !path.exists() {
-                        path = std::env::current_dir().unwrap().join(args_part);
-                    }
-                    // exist
-                    if !path.exists() {
-                        eprintln!("cd: {}: No such file or directory", path.to_str().unwrap());
-                    } else {
-                        // update path
-                        let update_working_dir_result = std::env::set_current_dir(path);
-                        if update_working_dir_result.is_err() {
-                            eprintln!(
-                                "Error on changing directory : {:?}",
-                                update_working_dir_result.err().unwrap()
-                            );
-                        }
-                    }
-                }
+                ShellCommands::Exit => shell_exit(args_part),
+                ShellCommands::Echo => println!("{}", args_part),
+                ShellCommands::Type => shell_type_command(&paths, args_part),
+                ShellCommands::PrintWorkingDirectory => shell_pwd_command(),
+                ShellCommands::ChangeDirectory => shell_cd_command(args_part),
             },
-            ShellCommandType::Unknow => {
-                println!("{}: command not found", command_part);
-            }
-            ShellCommandType::Program(path) => {
-                let mut program = std::process::Command::new(path);
-                program.envs(std::env::vars());
-                if !args_part.is_empty() {
-                    program.arg(args_part);
-                }
-                let program = program.spawn();
-                if let Ok(mut program) = program {
-                    match program.wait() {
-                        Ok(exit_code) => {
-                            if !exit_code.success() {
-                                eprintln!("Exit Fail : {}", exit_code);
-                            }
-                        }
-                        Err(e) => {
-                            eprintln!("Program crash: {:?}", e);
-                        }
-                    }
-                } else {
-                    eprintln!("Program Fail to start: {:?}", program.err().unwrap());
-                }
-            }
+            ShellCommandType::Unknow => println!("{}: command not found", command_part),
+            ShellCommandType::Program(path) => shell_run_program(path, args_part),
         }
         std::io::stdout().flush().unwrap();
     }
+}
+
+fn shell_run_program(path: String, args_part: &str) {
+    let mut program = std::process::Command::new(path);
+    program.envs(std::env::vars());
+    if !args_part.is_empty() {
+        program.arg(args_part);
+    }
+    let program = program.spawn();
+    if let Ok(mut program) = program {
+        match program.wait() {
+            Ok(exit_code) => {
+                if !exit_code.success() {
+                    eprintln!("Exit Fail : {}", exit_code);
+                }
+            }
+            Err(e) => {
+                eprintln!("Program crash: {:?}", e);
+            }
+        }
+    } else {
+        eprintln!("Program Fail to start: {:?}", program.err().unwrap());
+    }
+}
+
+fn shell_cd_command(args_part: &str) {
+    let mut path = std::path::PathBuf::new();
+    path.push(args_part);
+    // may need to be joined to working dir
+    if !path.exists() {
+        path = std::env::current_dir().unwrap().join(args_part);
+    }
+    // exist
+    if !path.exists() {
+        eprintln!("cd: {}: No such file or directory", path.to_str().unwrap());
+    } else {
+        // update path
+        let update_working_dir_result = std::env::set_current_dir(path);
+        if update_working_dir_result.is_err() {
+            eprintln!(
+                "Error on changing directory : {:?}",
+                update_working_dir_result.err().unwrap()
+            );
+        }
+    }
+}
+
+fn shell_pwd_command() {
+    let current_dir = std::env::current_dir();
+    if current_dir.is_err() {
+        eprintln!("Can't get current working directory: {:?}", current_dir.unwrap_err());
+    } else {
+        let current_dir = current_dir.unwrap();
+        println!("{}", current_dir.to_str().unwrap_or("UNKNOWN"));
+    }
+}
+
+fn shell_type_command(paths: &Vec<&std::path::Path>, args_part: &str) {
+    let arg_command_type = pase_command_type(&paths, args_part);
+    match arg_command_type {
+        ShellCommandType::Unknow => {
+            println!("{}: not found", args_part);
+        }
+        ShellCommandType::Program(path) => {
+            let path = absolute_path(&(std::path::Path::new(&path))).unwrap();
+            println!("{} is {}", args_part, path.to_str().unwrap());
+        }
+        ShellCommandType::Shell(_) => {
+            println!("{} is a shell builtin", args_part);
+        }
+    }
+}
+
+
+fn shell_exit(args_part: &str) {
+    let is_args_emp = args_part.is_empty();
+    let mut exit_code: i32 = args_part.parse().unwrap_or(-1);
+    if is_args_emp {
+        exit_code = 0;
+    }
+    std::process::exit(exit_code);
 }
