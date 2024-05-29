ef22bc95ee82dfc6b7f9c21aad5e283075826d90
Timbre Freeman <timbre@tftinker.tech>
2024-05-29 15:58:40 -0700

    cleanup - comments

--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,6 @@
 use std::io::Write;
 
-enum ShellCommandType {
-    Shell(ShellCommands),
-    Program(String),
-    Unknow,
-}
-
-#[derive(Debug)]
-enum ShellCommands {
-    Exit,
-    Echo,
-    Type,
-    PrintWorkingDirectory,
-    ChangeDirectory,
-}
-
-impl ShellCommands {
-    const VALUES: [Self; 5] = [
-        Self::Exit,
-        Self::Echo,
-        Self::Type,
-        Self::PrintWorkingDirectory,
-        Self::ChangeDirectory,
-    ];
-}
-
-impl std::fmt::Display for ShellCommands {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            ShellCommands::Exit => write!(f, "exit"),
-            ShellCommands::Echo => write!(f, "echo"),
-            ShellCommands::Type => write!(f, "type"),
-            ShellCommands::PrintWorkingDirectory => write!(f, "pwd"),
-            ShellCommands::ChangeDirectory => write!(f, "cd"),
-        }
-    }
-}
-
-fn pase_command_type(paths: &Vec<&std::path::Path>, command: &str) -> ShellCommandType {
-    // pase command
-    let mut input_command_type = ShellCommandType::Unknow;
-    for shell_command in ShellCommands::VALUES {
-        if shell_command.to_string().eq(command) {
-            input_command_type = ShellCommandType::Shell(shell_command);
-            break;
-        }
-    }
-    if let ShellCommandType::Unknow = input_command_type {
-        if std::path::Path::new(command).exists() {
-            input_command_type = ShellCommandType::Program(String::from(command));
-        }
-    }
-    if let ShellCommandType::Unknow = input_command_type {
-        for path in paths {
-            let command_path = path.join(command);
-            if command_path.exists() {
-                input_command_type =
-                    ShellCommandType::Program(command_path.to_str().unwrap().into());
-                break;
-            }
-        }
-    }
-    input_command_type
-}
-
-fn absolute_path(path: &std::path::Path) -> std::io::Result<std::path::PathBuf> {
-    let absolute_path = if path.is_absolute() {
-        path.to_path_buf()
-    } else {
-        std::env::current_dir()?.join(path)
-    };
-
-    let mut out = Vec::new();
-
-    use std::path::Component;
-
-    for comp in absolute_path.components() {
-        match comp {
-            Component::CurDir => (),
-            Component::ParentDir => match out.last() {
-                Some(Component::RootDir) => (),
-                Some(Component::Normal(_)) => {
-                    out.pop();
-                }
-                None
-                | Some(Component::CurDir)
-                | Some(Component::ParentDir)
-                | Some(Component::Prefix(_)) => out.push(comp),
-            },
-            comp => out.push(comp),
-        }
-    }
-
-    let clean_path = if !out.is_empty() {
-        out.iter().collect()
-    } else {
-        std::path::PathBuf::from(".")
-    };
-
-    Ok(clean_path)
-}
-
+/// # Start of the shell program
 fn main() {
     // path
     let env_path = std::env::var("PATH").unwrap_or(String::from(""));
@@ -127,7 +27,7 @@ fn main() {
         let args_part = args_part.trim();
 
         // run command
-        match pase_command_type(&paths, command_part) {
+        match parse_command_type(&paths, command_part) {
             ShellCommandType::Shell(shell_command) => match shell_command {
                 ShellCommands::Exit => shell_exit(args_part),
                 ShellCommands::Echo => println!("{}", args_part),
@@ -135,13 +35,140 @@ fn main() {
                 ShellCommands::PrintWorkingDirectory => shell_pwd_command(),
                 ShellCommands::ChangeDirectory => shell_cd_command(args_part),
             },
-            ShellCommandType::Unknow => println!("{}: command not found", command_part),
+            ShellCommandType::Unknown => println!("{}: command not found", command_part),
             ShellCommandType::Program(path) => shell_run_program(path, args_part),
         }
         std::io::stdout().flush().unwrap();
     }
 }
 
+/// # Shell Command Type
+///
+/// Enum of what type of command
+enum ShellCommandType {
+    Shell(ShellCommands),
+    Program(String),
+    Unknown,
+}
+
+/// # Shell Built In Commands
+///
+/// Enum of all shell's built in commands
+#[derive(Debug)]
+enum ShellCommands {
+    Exit,
+    Echo,
+    Type,
+    PrintWorkingDirectory,
+    ChangeDirectory,
+}
+
+// Java enum VALUES
+impl ShellCommands {
+    const VALUES: [Self; 5] = [
+        Self::Exit,
+        Self::Echo,
+        Self::Type,
+        Self::PrintWorkingDirectory,
+        Self::ChangeDirectory,
+    ];
+}
+
+// to_string
+impl std::fmt::Display for ShellCommands {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ShellCommands::Exit => write!(f, "exit"),
+            ShellCommands::Echo => write!(f, "echo"),
+            ShellCommands::Type => write!(f, "type"),
+            ShellCommands::PrintWorkingDirectory => write!(f, "pwd"),
+            ShellCommands::ChangeDirectory => write!(f, "cd"),
+        }
+    }
+}
+
+/// # Get Command Type
+///
+/// parse the command to get the type of command
+fn parse_command_type(paths: &Vec<&std::path::Path>, command: &str) -> ShellCommandType {
+    // pase command
+    let mut input_command_type = ShellCommandType::Unknown;
+    // is shell command?
+    for shell_command in ShellCommands::VALUES {
+        if shell_command.to_string().eq(command) {
+            input_command_type = ShellCommandType::Shell(shell_command);
+            break;
+        }
+    }
+    // is relative/absolute program command?
+    if let ShellCommandType::Unknown = input_command_type {
+        if std::path::Path::new(command).exists() {
+            input_command_type = ShellCommandType::Program(String::from(command));
+        }
+    }
+    // is path program command?
+    if let ShellCommandType::Unknown = input_command_type {
+        for path in paths {
+            let command_path = path.join(command);
+            if command_path.exists() {
+                input_command_type =
+                    ShellCommandType::Program(command_path.to_str().unwrap().into());
+                break;
+            }
+        }
+    }
+    // return type
+    input_command_type
+}
+
+/// # absolute path
+///
+/// Convert a path to an absolute path
+fn absolute_path(path: &std::path::Path) -> std::io::Result<std::path::PathBuf> {
+    // Make a valid path
+    let absolute_path = if path.is_absolute() {
+        path.to_path_buf()
+    } else {
+        std::env::current_dir()?.join(path)
+    };
+
+    // Clean path
+    let mut out = Vec::new();
+
+    use std::path::Component;
+
+    for comp in absolute_path.components() {
+        match comp {
+            Component::CurDir => (),
+            Component::ParentDir => match out.last() {
+                Some(Component::RootDir) => (),
+                Some(Component::Normal(_)) => {
+                    out.pop();
+                }
+                None
+                | Some(Component::CurDir)
+                | Some(Component::ParentDir)
+                | Some(Component::Prefix(_)) => out.push(comp),
+            },
+            comp => out.push(comp),
+        }
+    }
+
+    // Is there any path left
+    let clean_path = if !out.is_empty() {
+        out.iter().collect()
+    } else {
+        // This will **never** be that case as we are getting the absolute path
+        std::path::PathBuf::from(".")
+    };
+
+    // Return the cleanup absolute path
+    Ok(clean_path)
+}
+
+/// # Shell run program
+///
+/// Run the external program and hand any problems.
 fn shell_run_program(path: String, args_part: &str) {
     let mut program = std::process::Command::new(path);
     program.envs(std::env::vars());
@@ -165,6 +192,9 @@ fn shell_run_program(path: String, args_part: &str) {
     }
 }
 
+/// # Shell Change Directory Command
+///
+/// Change the current working directory of this program (shell)
 fn shell_cd_command(args_part: &str) {
     let mut path = std::path::PathBuf::new();
     path.push(args_part);
@@ -197,10 +227,14 @@ fn shell_pwd_command() {
     }
 }
 
+/// # Shell Command Type
+///
+/// The `type` builtin is used to determine how a command would be interpreted if used.
+/// **(for the user)**
 fn shell_type_command(paths: &Vec<&std::path::Path>, args_part: &str) {
-    let arg_command_type = pase_command_type(&paths, args_part);
+    let arg_command_type = parse_command_type(&paths, args_part);
     match arg_command_type {
-        ShellCommandType::Unknow => {
+        ShellCommandType::Unknown => {
             println!("{}: not found", args_part);
         }
         ShellCommandType::Program(path) => {
@@ -213,10 +247,21 @@ fn shell_type_command(paths: &Vec<&std::path::Path>, args_part: &str) {
     }
 }
 
-
-fn shell_exit(args_part: &str) {
-    let is_args_emp = args_part.is_empty();
-    let mut exit_code: i32 = args_part.parse().unwrap_or(-1);
+/// # Shell Command Exit
+///
+/// cause the shell to exit
+///
+/// > The `exit` utility shall cause the shell to exit from its current execution environment
+/// > with the exit status specified by the unsigned decimal integer n. If the current execution
+/// > environment is a subshell environment, the shell shall exit from the subshell environment
+/// > with the specified exit status and continue in the environment from which that subshell
+/// > environment was invoked; otherwise, the shell utility shall terminate with the specified
+/// > exit status. If n is specified, but its value is not between `0` and `255` inclusively,
+/// > the exit status is undefined.
+/// **From** [Open Group IEEE](https://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#exit)
+fn shell_exit(exit_code_str: &str) {
+    let is_args_emp = exit_code_str.is_empty();
+    let mut exit_code: i32 = exit_code_str.parse().unwrap_or(-1);
     if is_args_emp {
         exit_code = 0;
     }
